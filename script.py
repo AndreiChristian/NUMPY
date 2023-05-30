9dd9bdf84f37898f16672d5d207e050dd64fa835
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-05-30 12:16:42 +0200

    getting started with numpy

---
+++ script.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a1 = np.array([1,2,3])
+a2 = np.zeros(10)
+a3 = np.ones(10)
+a4 = np.random.random(10)
+a5 = np.random.randn(10)
+a6 = np.linspace(0,10,100)
+
+print(2*a1)
+print(1/a1)
