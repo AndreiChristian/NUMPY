d90b25e2d49a4d50d8d2a59cd2a67e2b50f1b867
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-16 17:58:10 +0200

    more work with np

--- data_types_and_attributes.py
+++ data_types_and_attributes.py
@@ -2,4 +2,12 @@ import numpy as np
 
 a1 = np.array([1,2,3])
 
-print(type(a1))
+a2 = np.array([[1,2],[3,4]])
+
+a3 = np.array([[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]]])
+
+ones = np.ones((10,10))
+
+zeros = np.zeros((3,5))
+
+print(zeros)
